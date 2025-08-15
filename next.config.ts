e89f5e6fc5f554c17969cb4065f3e4629298ca85
ibthecoder <oladipupoibrahim10@gmail.com>
2025-08-15 03:44:24 +0100

    changes made

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    turbopack: false, // Disable Turbopack for build
+  },
   /* config options here */
 };
 
